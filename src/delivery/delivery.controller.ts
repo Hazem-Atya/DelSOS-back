fec1824bc003ada64442588f25188f96d3896bb0
Hazem-Atya <53778545+Hazem-Atya@users.noreply.github.com>
2022-01-25 00:15:52 +0100

    Update delivery.controller.ts

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -1,16 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Patch,
-  Post,
-  Query,
-  Req,
-  UnauthorizedException,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, NotFoundException, Patch, Post, Query, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtAuthGuard } from 'src/auth/auth-guards/jwt-auth.guard';
 import { GetUser } from 'src/auth/decorators/user.decorator';
@@ -25,137 +13,143 @@ import { DeliveryService } from './service/delivery.service';
 
 @Controller('delivery')
 export class DeliveryController {
-  constructor(private deliveryService: DeliveryService) {}
+    constructor(private deliveryService: DeliveryService) { }
 
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  @UseGuards(JwtAuthGuard)
-  async addDelivery(
-    @Body() createDeliveryDTO: CreateDeliveryDTO,
-    @Req() request,
-  ) {
-    const store = request.user;
 
-    if (store.role != 'STORE') {
-      throw new UnauthorizedException();
-    }
-    console.log('This is the store I received:', store._id);
-    return await this.deliveryService.addDelivery(store._id, createDeliveryDTO);
-  }
 
-  @Get('all')
-  @UseGuards(JwtAuthGuard)
-  async getToresDeliveries(@Req() request) {
-    const store = request.user;
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @UseGuards(JwtAuthGuard)
+    async addDelivery(
+        @Body() createDeliveryDTO: CreateDeliveryDTO,
+        @Req() request
+    ) {
+        const store = request.user;
 
-    return await this.deliveryService.getDeliveriesByStoreId(store._id);
-  }
-
-  // get the deliveris that have been delivered
-  @Get('archive')
-  @UseGuards(JwtAuthGuard)
-  async getStoresArchivedDeliveries(
-    @GetUser() store,
-    @Query() { skip, limit }: PaginationParams,
-  ) {
-    return await this.deliveryService.getArchivedDeliveriesByStoreId(
-      store._id,
-      skip,
-      limit,
-    );
-  }
-  @Patch('affect-shopper')
-  @UseGuards(JwtAuthGuard)
-  async affectShopperToDelivery(
-    @Req() request,
-    @Body() delivery_shopper_data: AffectShopperDTO,
-  ) {
-    const store = request.user;
-    return await this.deliveryService.affectShoppertoDelivery(
-      store._id,
-      delivery_shopper_data.shopperEmail,
-      delivery_shopper_data.deliveryId,
-    );
-  }
-
-  @Patch('apply')
-  @UseGuards(JwtAuthGuard)
-  async requestDelivery(
-    @GetUser() shopper,
-    @Body() delivery: UpdateDeliveryDTO,
-  ) {
-    if (shopper.role != ROLE.shopper) {
-      throw new UnauthorizedException('nooooooooooooooo');
-    }
-    return this.deliveryService.requestDelivery(
-      delivery.deliveryId,
-      shopper._id,
-    );
-  }
-
-  @Get('shoppersDeliveries')
-  @UseGuards(JwtAuthGuard)
-  async getShopperDeliveries(
-    @GetUser() shopper,
-    @Query() { skip, limit }: PaginationParams,
-  ) {
-    if (shopper.role != ROLE.shopper) {
-      throw new UnauthorizedException('WE NEED A SHOPPER');
-    }
-    return await this.deliveryService.getDeliveriesByShopperId(
-      shopper._id,
-      skip,
-      limit,
-    );
-  }
-  @Patch('markAsDelivered')
-  @UseGuards(JwtAuthGuard)
-  async markAsDeliverd(
-    @GetUser() shopper,
-    @Body() delivery: UpdateDeliveryDTO,
-  ) {
-    if (shopper.role != ROLE.shopper) {
-      throw new UnauthorizedException('WE NEED A SHOPPER');
+        if (store.role != 'STORE') {
+            throw new UnauthorizedException();
+        }
+        console.log("This is the store I received:", store._id)
+        return await this.deliveryService.addDelivery(store._id, createDeliveryDTO);
     }
 
-    return await this.deliveryService.markDeliveryAsDelivered(
-      shopper._id,
-      delivery.deliveryId,
-    );
-  }
+    @Get('all')
+    @UseGuards(JwtAuthGuard)
+    async getToresDeliveries(
+        @Req() request
+    ) {
+        const store = request.user;
 
-  @Get('requestedDeliveries')
-  @UseGuards(JwtAuthGuard)
-  getRequestedDeliveries(
-    @GetUser() shopper: Partial<Shopper>,
-    @Query() { skip, limit }: PaginationParams,
-  ) {
-    if (shopper.role != ROLE.shopper) {
-      throw new UnauthorizedException(
-        'This route can only be accessed by a shopper',
-      );
-    }
-    return this.deliveryService.getRequestedDeliveries(
-      shopper._id,
-      skip,
-      limit,
-    );
-  }
+        return await this.deliveryService.getDeliveriesByStoreId(store._id);
 
-  @Patch('addTrackingData')
-  @UseGuards(JwtAuthGuard)
-  async addTrackingHistory(
-    @GetUser() shopper,
-    @Body() addTrackingData: AddTrackingDTO,
-  ) {
-    if (shopper.role != ROLE.shopper) {
-      throw new UnauthorizedException(
-        'This route can only be accessed by a shopper',
-      );
     }
-    return await this.deliveryService.addTrackingData(
-      shopper._id,
-      addTrackingData,
-    );
-  }
+
+    // get the deliveris that have been delivered
+    @Get('archive')
+    @UseGuards(JwtAuthGuard)
+    async getStoresArchivedDeliveries(
+        @GetUser() store,
+        @Query() { skip, limit }: PaginationParams
+    ) {
+        return await this.deliveryService.
+            getArchivedDeliveriesByStoreId(store._id, skip, limit);
+
+    }
+    @Patch('affect-shopper')
+    @UseGuards(JwtAuthGuard)
+    async affectShopperToDelivery(
+        @Req() request,
+        @Body() delivery_shopper_data: AffectShopperDTO
+    ) {
+
+        const store = request.user;
+        return await this.deliveryService.affectShoppertoDelivery(
+            store._id,
+            delivery_shopper_data.shopperEmail,
+            delivery_shopper_data.deliveryId);
+    }
+
+
+    @Patch('remove-shopper')
+    @UseGuards(JwtAuthGuard)
+    async removeShopperFromDelivery(
+        @Req() request,
+        @Body() delivery_shopper_data: AffectShopperDTO
+    ) {
+
+        const store = request.user;
+        return await this.deliveryService.removeShopperFromDelivery(
+            store._id,
+            delivery_shopper_data.shopperEmail,
+            delivery_shopper_data.deliveryId);
+    }
+
+
+
+    @Patch('apply')
+    @UseGuards(JwtAuthGuard)
+    async requestDelivery(
+        @GetUser() shopper,
+        @Body() delivery: UpdateDeliveryDTO
+    ) {
+
+        if (shopper.role != ROLE.shopper) {
+            throw new UnauthorizedException("nooooooooooooooo");
+        }
+        return this.deliveryService.requestDelivery(delivery.deliveryId, shopper._id);
+    }
+
+
+    @Get('shoppersDeliveries')
+    @UseGuards(JwtAuthGuard)
+    async getShopperDeliveries(
+        @GetUser() shopper,
+        @Query() { skip, limit }: PaginationParams
+
+    ) {
+
+        if (shopper.role != ROLE.shopper) {
+            throw new UnauthorizedException("WE NEED A SHOPPER");
+        }
+        return await this.deliveryService.getDeliveriesByShopperId(shopper._id, skip, limit);
+    }
+    @Patch('markAsDelivered')
+    @UseGuards(JwtAuthGuard)
+    async markAsDeliverd(
+        @GetUser() shopper,
+        @Body() delivery: UpdateDeliveryDTO
+    ) {
+
+        if (shopper.role != ROLE.shopper) {
+            throw new UnauthorizedException("WE NEED A SHOPPER");
+        }
+
+        return await this.deliveryService.markDeliveryAsDelivered(shopper._id, delivery.deliveryId);
+    }
+
+    @Get('requestedDeliveries')
+    @UseGuards(JwtAuthGuard)
+    getRequestedDeliveries(
+        @GetUser() shopper: Partial<Shopper>,
+        @Query() { skip, limit }: PaginationParams
+
+    ) {
+        if (shopper.role != ROLE.shopper) {
+            throw new UnauthorizedException('This route can only be accessed by a shopper');
+        }
+        return this.deliveryService.getRequestedDeliveries(shopper._id, skip, limit);
+    }
+
+    @Patch('addTrackingData')
+    @UseGuards(JwtAuthGuard)
+    async addTrackingHistory(
+        @GetUser() shopper,
+        @Body() addTrackingData: AddTrackingDTO
+    ) {
+        if (shopper.role != ROLE.shopper) {
+            throw new UnauthorizedException('This route can only be accessed by a shopper');
+        }
+        return await this.deliveryService.addTrackingData(shopper._id, addTrackingData);
+    }
+
 }
